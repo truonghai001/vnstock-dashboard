42af25c1edbd3a5889c85aa366d761d9b54646d3
Hai Truong <haitruong3001@gmail.com>
2025-09-08 16:03:15 +0700

    display data in main page

--- app.py
+++ app.py
@@ -21,28 +21,9 @@ with st.sidebar:
     stock_list = get_list_of_watch_stocks(engine)
     selected_stock = st.selectbox("Select Ticker", stock_list, index=1)
     
-    st.write(selected_stock)
-    st.write("Stock ticker: ", selected_stock[:3])
     
-    
-st.markdown(
-    """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-"""
-)
+st.write(selected_stock)
+st.write("Stock ticker: ", selected_stock[:3])
 
-st.write("DB username:", st.secrets["DB_USERNAME"])
 
 
